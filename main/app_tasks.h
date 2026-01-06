2087bddeac896dd636771041c7cb11e58825d19d
rootquan <530637626@qq.com>
2026-01-06 08:26:41 +0800

    xgcg

---
+++ main/app_tasks.h
@@ -0,0 +1,22 @@
+#ifndef __APP_TASKS_H__
+#define __APP_TASKS_H__
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+/* 任务优先级定义 */
+#define TASK1_PRIORITY (tskIDLE_PRIORITY + 1)
+#define TASK2_PRIORITY (tskIDLE_PRIORITY + 2)
+
+/* 任务栈大小定义 */
+#define TASK1_STACK_SIZE (2048)
+#define TASK2_STACK_SIZE (2048)
+
+/* 队列句柄声明 */
+extern QueueHandle_t g_data_queue;
+
+/* 任务创建函数 */
+void app_tasks_create(void);
+
+#endif
